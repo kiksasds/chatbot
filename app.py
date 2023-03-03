d1df2c31338ebc1e8dcb7a46fb771272de548852
kiksasds <126202327+kiksasds@users.noreply.github.com>
2023-03-03 20:49:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import random
-from chat import get_response, intents, tags, get_fallback_subject_index
+from chat import get_response
 
 app = Flask(__name__)
 
@@ -15,26 +15,6 @@ def predict():
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
-
-
-@app.post("/fallback")
-def fallback():
-
-    
-    subject = tags[get_fallback_subject_index()]
-
-    response = f"Do you mean '{subject}'? (yes or no)"
-
-    user_response = input().lower()
-    if user_response == "yes":
-        for intent in intents["intents"]:
-            if intent["tag"] == subject:
-                response = random.choice(intent["responses"])
-                break
-    else:
-        response = "Please rephrase your question."
-
-    return jsonify({'answer': response})
 
 
 if __name__ == "__main__":
