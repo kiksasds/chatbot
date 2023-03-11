03454e33b3137c27556561909a0b8891553c3a9e
kiksasds <126202327+kiksasds@users.noreply.github.com>
2023-03-10 22:54:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import json
 from chat import get_response
 
 app = Flask(__name__)
 
+with open('intents.json', 'r', encoding='utf-8') as json_data:
+    intents = json.load(json_data)
+
+tag = None
 
 @app.get("/")
 def index_get():
     return render_template("base.html")
 
+
 @app.post("/predict")
 def predict():
+    global tag
     text = request.get_json().get("message")
-    response = get_response(text)
+    response, predicted_tag = get_response(text)
+    tag = predicted_tag
     message = {"answer": response}
     return jsonify(message)
+
+
+@app.post("/fallback")
+def fallback():
+    global tag
+    text = request.get_json().get("message")
+    if text == 'yes':
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                response = random.choice(intent['responses'])
+                message = {"answer": response}
+                return jsonify(message)
+    else:
+        response = "Reescreva a pergunta..."
+        message = {"answer": response}
+        return jsonify(message)
 
 
 if __name__ == "__main__":
