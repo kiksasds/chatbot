68ae3a7248cc332000d621a1243670830897e307
sepp <seppdblack@gmail.com>
2023-06-05 17:26:53 -0300

    Colocado Similaridade no "chat.py" e adicionado novas informações no "intents.json"

--- chat.py
+++ chat.py
@@ -2,9 +2,10 @@ import random
 import json
 
 import torch
-
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+from nltk_utils import bag_of_words, tokenize, stem
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -41,12 +42,26 @@ def get_response(msg):
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    if prob.item() > 0.99:
+
+    if prob.item() > 0.8:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses']), tag
     else:
-        return f"Desculpe, não entendi o que você quis dizer. Você perguntou sobre '{tag}'...?", tag
+        similarities = []
+        for intent in intents['intents']:
+            intent_words = [stem(word) for word in tokenize(intent['patterns'][0])]
+            intent_vector = bag_of_words(intent_words, all_words)
+            similarity = cosine_similarity(X, intent_vector.reshape(1, -1))
+            similarities.append(similarity)
+        max_similarity_idx = np.argmax(similarities)
+        max_similarity = similarities[max_similarity_idx]
+
+        if max_similarity > 0.5:
+            tag = intents['intents'][max_similarity_idx]["tag"]
+            return f"Desculpe, não tenho certeza sobre isso. Você está se referindo a '{tag}'?", tag
+        else:
+            return f"Desculpe, não entendi o que você quis dizer. Você perguntou sobre '{tag}'...?", tag
 
 
 if __name__ == "__main__":
